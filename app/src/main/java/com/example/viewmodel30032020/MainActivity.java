5239092022a7177673bbf282cd080f94792b0eb4
phatdroid94 <phatdroid94@gmail.com>
2020-06-12 20:55:08 +0700

    update code

--- app/src/main/java/com/example/viewmodel30032020/MainActivity.java
+++ app/src/main/java/com/example/viewmodel30032020/MainActivity.java
@@ -10,26 +10,28 @@ import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    MainViewModel mainViewModel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mainViewModel = new MainViewModel();
         // Observe : Pham vi lang nghe du lieu
-        MainViewModel
-                .getInstance()
+        mainViewModel
                 .getDataRandom()
                 .observe(this, new Observer<Integer>() {
-            @Override
-            public void onChanged(Integer integer) {
-                if (integer != null){
-                    Log.d("BBB",integer + "");
-                }else {
-                    Log.d("BBB","null");
-                }
-            }
-        });
+                    @Override
+                    public void onChanged(Integer integer) {
+                        if (integer != null) {
+                            Log.d("BBB", integer + "");
+                        } else {
+                            Log.d("BBB", "null");
+                        }
+                    }
+                });
         // setListener : Call data trong main
-        MainViewModel.getInstance().randomNumber(10);
+        mainViewModel.randomNumber(10);
     }
 }
