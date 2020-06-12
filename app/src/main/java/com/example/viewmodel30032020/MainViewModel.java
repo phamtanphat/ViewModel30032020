156d0c2aa9d03cfce0cff78cd928ea4c67aed0c0
phatdroid94 <phatdroid94@gmail.com>
2020-06-12 21:11:39 +0700

    ui

--- app/src/main/java/com/example/viewmodel30032020/MainViewModel.java
+++ app/src/main/java/com/example/viewmodel30032020/MainViewModel.java
@@ -1,35 +1,20 @@
 package com.example.viewmodel30032020;
 
-import android.util.Log;
-
-import androidx.lifecycle.Lifecycle;
-import androidx.lifecycle.LifecycleObserver;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.OnLifecycleEvent;
 import androidx.lifecycle.ViewModel;
 
-import java.util.Random;
-
-public class MainViewModel extends ViewModel implements LifecycleObserver {
-    private MutableLiveData<Integer> mDataRandom;
+public class MainViewModel extends ViewModel {
+   private MutableLiveData<String> mDataString;
 
     public MainViewModel() {
-        this.mDataRandom = new MutableLiveData<>();
+        this.mDataString = new MutableLiveData<>();
     }
 
-    public void randomNumber(int bound){
-        Random random = new Random();
-        int value = random.nextInt(bound);
-        mDataRandom.setValue(value);
+    public void setDataString(String chuoi){
+        mDataString.setValue(chuoi);
     }
-    public LiveData<Integer> getDataRandom(){
-        return mDataRandom;
+    public LiveData<String> getDataString(){
+        return  mDataString;
     }
-
-    @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
-    public void onCreate(){
-        Log.d("BBB","onCreate");
-    }
-
 }
